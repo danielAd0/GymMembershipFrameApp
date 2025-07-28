8e1362c93f68d78c1c20cebb9159871a96e934f4
Daniel <145106436+danielAd0@users.noreply.github.com>
2025-07-28 20:27:21 +0200

    Add files via upload

---
+++ GymMembershipFrameApp.java
@@ -0,0 +1,17 @@
+
+package gymmembershipframeapp;
+
+import za.ac.tut.ui.GymMembershipFrame;
+
+/**
+ *
+ * @author Daniel Adoration
+ */
+public class GymMembershipFrameApp {
+
+    public static void main(String[] args) {
+        //instantiation
+        new GymMembershipFrame();
+    }
+    
+}
